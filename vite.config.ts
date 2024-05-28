f3a140c8341bd966782385cf260b5cb84f9764ff
Wii Yang <wii_yang@163.com>
2024-05-28 17:00:11 +0800

    feat: 添加并配置 ant-design-vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dts: 'types/components.d.ts',
+      resolvers: [AntDesignVueResolver({ importStyle: 'less' })],
+    }),
+  ],
   css: {
     preprocessorOptions: {
       less: {
